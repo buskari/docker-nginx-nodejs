b1b1444bbb3fe23776ec2a647db00ec98d2b74b3
Andris Buscariolli <andris@DESKTOP-0042JBV.localdomain>
2022-10-02 18:49:21 -0300

    done

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,10 +1,62 @@
 const express = require('express')
+const mysql = require('mysql')
 
 const server = express()
 const PORT = 3001
+let data;
+
+const connection = mysql.createConnection({
+    host: 'db',
+    user: 'root',
+    password: 'admin',
+    database: 'nodedb'
+})
+
+const clean_data = `
+    DELETE 
+    FROM
+        developers;
+`
+
+const insert_data = `INSERT INTO developers (
+    first_name,
+    last_name,
+    role,
+    age
+) VALUES (
+    'Andris',
+    'Buscariolli',
+    'Software Developer',
+    33
+)`
+
+const retrieve_data = `
+    SELECT
+        *
+    FROM
+        developers;
+`
+
+connection.connect(err => {
+    if (err) throw err
+    console.log('Connected to the mysql')
+
+    connection.query(clean_data)
+
+    connection.query(insert_data, (err, result) => {
+        if (err) throw err
+        console.log('New record inserted')
+    })
+
+    connection.query(retrieve_data, (err, result, fields) => {
+        if (err) throw err
+        console.log(result);
+        data = result;
+    })
+})
 
 server.get('/', (req, res) => {
-    res.send('Hello from server')
+    res.send(data)
 })
 
 server.listen(PORT, () => {
